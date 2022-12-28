9792ddfb26c5441dc345d768b52864dd9c403332
Guillaume Rossignol <guillaume.rossignol@tutanota.com>
2022-12-28 23:42:50 +0100

    start basic UI with title and sample button

--- app/src/main/java/rg/info/storagelocator/MainActivity.kt
+++ app/src/main/java/rg/info/storagelocator/MainActivity.kt
@@ -6,10 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import rg.info.storagelocator.home.HomeComponent
 import rg.info.storagelocator.ui.theme.StorageLocatorTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,22 +17,9 @@ class MainActivity : ComponentActivity() {
             StorageLocatorTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    HomeComponent()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    StorageLocatorTheme {
-        Greeting("Android")
     }
 }
